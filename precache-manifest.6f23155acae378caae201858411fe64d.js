35c6ec2f13d8808929b8fa89a92f869e12801ac4
Ginger Balmat <gbalmat.ctr@kr.af.mil>
2019-09-05 01:02:08 -0400

    Updates

---
+++ precache-manifest.6f23155acae378caae201858411fe64d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b8a84fd7bb04f95ecf2983156ef05f",
+    "url": "/simple-url-app/index.html"
+  },
+  {
+    "revision": "bd8b631c84b240788178",
+    "url": "/simple-url-app/static/css/main.bb1498be.chunk.css"
+  },
+  {
+    "revision": "5dbcf6338dc2c7214163",
+    "url": "/simple-url-app/static/js/2.bfe2616b.chunk.js"
+  },
+  {
+    "revision": "bd8b631c84b240788178",
+    "url": "/simple-url-app/static/js/main.ef6a6b0b.chunk.js"
+  },
+  {
+    "revision": "736fb5d99c3e3fb5e3cb",
+    "url": "/simple-url-app/static/js/runtime~main.b8fecb32.js"
+  }
+]);
